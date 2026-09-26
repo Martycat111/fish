3a2b764bd7fc57e1e35a5268e9dece94335621fc
Martycat111 <walfie308@gmail.com>
2026-09-26 12:19:03 +1200

    dd

---
+++ scenes/main/hud_materialnotify.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+var notification_scene = load("res://scenes/notification/notification.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	notify("gaming", 3)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func notify(item_name: String, amount: int):
+	var instance = notification_scene.instantiate()
+	add_child(instance)
+	instance.set_text("+" + str(amount) + " " + item_name)
+	var anim = instance.get_node("AnimationPlayer")
+	anim.play("Note")
+	pass
