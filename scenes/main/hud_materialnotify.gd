ebfd65005e72ded735012e02a47244cea53eafb1
Martycat111 <walfie308@gmail.com>
2026-09-26 13:24:10 +1200

    e

--- scenes/main/hud_materialnotify.gd
+++ scenes/main/hud_materialnotify.gd
@@ -2,14 +2,22 @@ extends Node2D
 
 
 var notification_scene = load("res://scenes/notification/notification.tscn")
-
+var note_is_available = true
+signal gaming
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	notify("Example", 1)
+	pass
 
-
+func wait(seconds: float):
+	await get_tree().create_timer(seconds).timeout
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	pass
+
+func notify_array(items: Array):
+	for i in items:
+		notify(i, 1)
+		await 
 	pass
 
 func notify(item_name: String, amount: int):
@@ -21,3 +29,4 @@ func notify(item_name: String, amount: int):
 	anim.play("Note")
 	await anim.animation_finished
 	instance.queue_free()
+	emit_signal("gaming")
