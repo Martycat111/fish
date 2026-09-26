4254fff43e0020a8276908855fb76a7c05ba8e64
Wizard-Lizard36 <max@bartle.co.nz>
2026-09-26 17:01:09 +1200

    please work

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+@export var breakable_panel: TileMapLayer
+@export var breakable_electrical: TileMapLayer
+@export var breakable_glass: TileMapLayer
+
+
 var jump_velocity: float = 500
 var speed: float = 10
 var frozen: bool = true
@@ -13,6 +18,20 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= jump_velocity
+			
+	if Input.is_action_just_pressed("repair"):
+		var rounded_position: Vector2i = (position / 32).floor()
+		if breakable_panel.get_used_cells().has(rounded_position):
+			breakable_panel.erase_cell(rounded_position)
+			breakable_panel.set_cell(rounded_position, 0, Vector2(1, 1))
+			
+		if breakable_electrical.get_used_cells().has(rounded_position):
+			breakable_electrical.erase_cell(rounded_position)
+			breakable_electrical.set_cell(rounded_position, 0, Vector2(3, 1))
+		
+		if breakable_glass.get_used_cells().has(rounded_position):
+			breakable_glass.erase_cell(rounded_position)
+			breakable_glass.set_cell(rounded_position, 0, Vector2(2, 2))
 	
 	velocity.x -= speed * (Input.get_action_strength("move_left") - Input.get_action_strength("move_right"))
 	
