921576fe14c9850984013b374ef1da6b40eb4f60
Martycat111 <walfie308@gmail.com>
2026-09-26 13:24:26 +1200

    e

--- scenes/main/hud_materialnotify.gd
+++ scenes/main/hud_materialnotify.gd
@@ -17,7 +17,11 @@ func _process(_delta: float) -> void:
 func notify_array(items: Array):
 	for i in items:
 		notify(i, 1)
+<<<<<<< Updated upstream
 		await 
+=======
+		wait(1000)
+>>>>>>> Stashed changes
 	pass
 
 func notify(item_name: String, amount: int):
