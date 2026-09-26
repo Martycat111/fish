7fc69087cfd34b2c017c6d36989c6cebd7fae83e
Wizard-Lizard36 <max@bartle.co.nz>
2026-09-26 14:53:53 +1200

    Interior stuff

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
-var jump_velocity: float = 10
+var jump_velocity: float = 500
 var speed: float = 10
+var frozen: bool = false
 
 func _physics_process(delta: float) -> void:
+	if frozen:
+		return
+	
 	
 	velocity += get_gravity() * delta
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y += jump_velocity
+		velocity.y -= jump_velocity
 	
-	velocity.x += speed * (Input.get_action_strength("move_left") - Input.get_action_strength("move_right"))
+	velocity.x -= speed * (Input.get_action_strength("move_left") - Input.get_action_strength("move_right"))
 	
 	move_and_slide()
--- scenes/player/player_ship.gd
+++ scenes/player/player_ship.gd
@@ -1,10 +1,30 @@
 extends CharacterBody2D
 
+@export var player: CharacterBody2D
+@export var fish_cover: Sprite2D
+@export var ship_interior: ShipInterior
+@export var wall_layer_1: TileMapLayer
+@export var wall_layer_2: TileMapLayer
+
 var speed: float = 10
 var rotation_speed: float = 2
 
 
+
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_ship_mode"):
+		player.frozen = !player.frozen
+		var ship_active: bool = player.frozen
+		
+		fish_cover.visible = ship_active
+		player.visible = !ship_active
+		ship_interior.enabled = !ship_active
+		wall_layer_1.enabled = !ship_active
+		wall_layer_2.enabled = !ship_active
+	
+	if !player.frozen:
+		return
+	
 	if Input.is_action_pressed("move_forward"):
 		velocity += speed * Vector2.RIGHT.rotated(rotation)
 	rotation_degrees -= rotation_speed * (Input.get_action_strength("rotate_ship_left") - Input.get_action_strength("rotate_ship_right"))
